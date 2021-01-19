5312a6518d55acb09d8cebef1dc1bad37ea27e71
Andiisulides <77681103+Andiisulides@users.noreply.github.com>
2021-01-19 21:50:48 +0700

    Membuat Form Help

--- Hangman/Help.Designer.cs
+++ Hangman/Help.Designer.cs
@@ -29,12 +29,61 @@ namespace Hangman
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cara bermain";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(349, 65);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1. Pada bagian Menu klik Mulai\r\n\r\n2. Jika sudah klik button yang telah disediakan" +
+    " abcd\r\n\r\n3. Game akan berakhir jika Sisa giliran habis atau berhasil menebak kat" +
+    "a";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 21);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(371, 149);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Help";
             this.Text = "Help";
+            this.Load += new System.EventHandler(this.Help_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
