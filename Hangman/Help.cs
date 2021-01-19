936185fb5dbfd1477919f5f377db7efe385d7527
Andiisulides <77681103+Andiisulides@users.noreply.github.com>
2021-01-19 21:53:51 +0700

    Code Help

--- Hangman/Help.cs
+++ Hangman/Help.cs
@@ -16,5 +16,17 @@ namespace Hangman
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Menu form = new Menu();
+            this.Hide();
+            form.Show();
+        }
+
+        private void Help_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
